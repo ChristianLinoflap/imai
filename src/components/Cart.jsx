8302caf740d946ec3341dc7fc7b01bdfeac42ecb
jacklinoflap <jack.bicera@linoflaptech.com>
2023-11-23 16:10:09 +0800

    Dashboard Changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,39 +1,83 @@
-import React from 'react'
-import Button, { styles } from './Button';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-const handleClick = () => {
-    window.location.href = '/';
-    
-    // You can add your desired functionality here
-  };
-function Cart(){
-    const items = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11','11','11','11','11','11','11','11','11','11','11']; // Example array of items
+import React, { useState } from "react";
+import Button, { styles } from "./Button";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+function Cart() {
+    const items = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "11",
+        "11",
+        "11",
+        "11",
+        "11",
+        "11",
+        "11",
+        "11",
+        "11",
+        "11",
+    ]; // Example array of items
 
+    const [modalState, setModalState] = useState(false);
+    function openModal() {
+        setModalState(true);
+    }
+    function closeModal() {
+        setModalState(false);
+    }
     return (
-     
-    <div className='Inventory-Grid'>
-      <div style={{ marginInline: '20px'}}>
-        <h2>Cart Overview</h2>
-      </div>
-          <div className='Cart-Grid'>
-            <div className='Divider'>
-              <div className='Cart-Button'>
-                <Button onClick={handleClick} buttonStyle={styles.button} title="Add Cart" />
+        <div className="Inventory-Grid">
+            <Modal
+                open={modalState}
+                onClose={closeModal}
+                center
+            >
+              <div>
+                
               </div>
-              <div className='Card-1'>
-                {items.map((item, index) => (
-                <div key={index} className='Box-1'>
-                <h2>CART</h2>   
-                <h1 id={`item-${index}`} >{item}</h1  >
-                <ShoppingCartOutlinedIcon style={{ margin: '0%', fontSize: 70, color: 'black' }} /> 
-                </div>
-              ))}
-              </div>
+            </Modal>
+            <div style={{ marginInline: "20px" }}>
+                <h2>Cart Overview</h2>
             </div>
-          </div>
-    </div>
-
-     
+            <div className="Cart-Grid">
+                <div className="Divider">
+                    <div className="Cart-Button">
+                        <Button
+                            onClick={() => {
+                                openModal();
+                            }}
+                            buttonStyle={styles.button}
+                            title="Add Cart"
+                        />
+                    </div>
+                    <div className="Card-1">
+                        {items.map((item, index) => (
+                            <div key={index} className="Box-1">
+                                <h2>CART</h2>
+                                <h1 id={`item-${index}`}>{item}</h1>
+                                <ShoppingCartOutlinedIcon
+                                    style={{
+                                        margin: "0%",
+                                        fontSize: 70,
+                                        color: "black",
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
-export default Cart
+export default Cart;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,36 +1,226 @@
-import React from 'react'
+import React from "react";
 
-function Dashboard(){
-    return(
-            <div className='Split-Dash'>
-            <div className='Left-Dash'>
-                <div className='Left-Grid-Dash'>
-                    <div className='Card-1'>
-                        <h2>Sales Overview</h2>
+function Dashboard() {
+    return (
+        <div className="dashboard-22313">
+            <p>{"Welcome (User)"}</p>
+            <div className="dashboard-contents">
+                <div className="content-group-1">
+                    <div className="sales-overview">
+                        <p>Sales Overview</p>
+                        <div className="content-container">
+                            <div className="content">
+                                <img src="/assets/sales.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Sales</p>
+                                </div>
+                            </div>
+
+                            <div className="content">
+                                <img src="/assets/revenue.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Revenue</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                <img src="/assets/profit.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Profit</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                <img src="/assets/costyen.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Cost</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className='Card-2'>
-                        <h2>Purchase Overview</h2>
+                    <div className="purchase-overview">
+                        <p>Purchase Overview</p>
+                        <div className="content-container">
+                            <div className="content">
+                                <img src="/assets/Purchase.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Purchase</p>
+                                </div>
+                            </div>
+
+                            <div className="content">
+                                <img src="/assets/Cost.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Cost</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                <img src="/assets/Categories.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Profit</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                <img src="/assets/Brands.png" />
+                                <div>
+                                    <p>¥ 832</p>
+                                    <p>Cost</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className='Card-3'>
-                        <h2>Top Selling Product</h2>
+                    <div className="top-product">
+                        <p>Top Selling Product</p>
+                        <table>
+                            <tr>
+                                <th>Name</th>
+                                <th>Sold Quantity</th>
+                                <th>Remaining Quantity</th>
+                                <th>Price</th>
+                            </tr>
+                            <tr>
+                                <td>Surf</td>
+                                <td>32</td>
+                                <td>42</td>
+                                <td>¥ 832</td>
+                            </tr>
+                            <tr>
+                                <td>Rin</td>
+                                <td>42</td>
+                                <td>15</td>
+                                <td>¥ 832</td>
+                            </tr>
+                            <tr>
+                                <td>Parle G</td>
+                                <td>12</td>
+                                <td>32</td>
+                                <td>¥ 832</td>
+                            </tr>
+                            <tr>
+                                <td>Honey</td>
+                                <td>32</td>
+                                <td>51</td>
+                                <td>¥ 832</td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
-                {/* <h2>Left</h2> */}
-            </div>
-            <div className='Right-Dash'>
-                <div className='Right-Grid-Dash'>
-                    <div className='Card-1'>
-                        <h2>Low Quantity Stock</h2>
+                <div className="content-group-2">
+                    <div className="low-quantity">
+                        <p>Low Quantity Stocks</p>
+                        <div className="content-container">
+                            <div className="content">
+                                <div>
+                                    <h2>Tata Salt</h2>
+                                    <p>
+                                        Remaining Quantity:<span>10</span>
+                                    </p>
+                                </div>
+                                <p>Low</p>
+                            </div>
+                            <div className="content">
+                                <div>
+                                    <h2>Lays</h2>
+                                    <p>
+                                        Remaining Quantity:<span>10</span>
+                                    </p>
+                                </div>
+                                <p>Low</p>
+                            </div>
+                            <div className="content">
+                                <div>
+                                    <h2>Maggi</h2>
+                                    <p>
+                                        Remaining Quantity:<span>10</span>
+                                    </p>
+                                </div>
+                                <p>Low</p>
+                            </div>
+                            <div className="content">
+                                <div>
+                                    <h2>Box</h2>
+                                    <p>
+                                        Remaining Quantity:<span>10</span>
+                                    </p>
+                                </div>
+                                <p>Low</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='Card-2'>
-                        <h2>Least Selling Product</h2>
+                    <div className="low-quantity">
+                        <p>Least Selling Product</p>
+                        <div className="content-container">
+                            <div className="content">
+                                <div>
+                                    <h2>Tata Salt</h2>
+                                    <p>
+                                        Sold Quantity: <span> 10 Pax</span>
+                                    </p>
+                                </div>
+                                <p>¥ 8</p>
+                            </div>
+                            <div className="content">
+                                <div>
+                                    <h2>Lays</h2>
+                                    <p>
+                                        Sold Quantity: <span> 2 Pax</span>
+                                    </p>
+                                </div>
+                                <p>¥ 8</p>
+                            </div>
+                            <div className="content">
+                                <div>
+                                    <h2>Maggi</h2>
+                                    <p>
+                                        Sold Quantity: <span> 3 Pax</span>
+                                    </p>
+                                </div>
+                                <p>¥ 2</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    
-        
-
-    )
+    );
 }
-export default Dashboard
+export default Dashboard;
+
+{
+    /* <div className="Split-Dash">
+            <div className="Left-Dash">
+                <div className="Left-Grid-Dash">
+                    <div className="Card-1">
+                        <h2>Sales Overview</h2>
+                        <div className="content">
+                        </div>
+                    </div>
+                    <div className="Card-2">
+                        <h2>Purchase Overview</h2>
+                        <div className="content"></div>
+                    </div>
+                    <div className="Card-3">
+                        <h2>Top Selling Product</h2>
+                        <div className="content"></div>
+                    </div>
+                </div>
+                </div>
+            <div className="Right-Dash">
+                <div className="Right-Grid-Dash">
+                    <div className="Card-1">
+                        <h2>Low Quantity Stock</h2>
+                        <div className="content"></div>
+                    </div>
+                    <div className="Card-2">
+                        <h2>Least Selling Product</h2>
+                        <div className="content"></div>
+                    </div>
+                </div>
+            </div>
+        </div> */
+}
